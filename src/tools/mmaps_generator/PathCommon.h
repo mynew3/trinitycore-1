a6b4e0855ceeee3bf8860c84bfe569c3474bdba0
3306665 <413681487@qq.com>
2013-11-05 07:05:00 +0800

    退回MMAP到V3

--- src/tools/mmaps_generator/PathCommon.h
+++ src/tools/mmaps_generator/PathCommon.h
@@ -114,6 +114,7 @@ namespace MMAP
         const char *p = dirpath.c_str();
         DIR * dirp = opendir(p);
         struct dirent * dp;
+        dirp = opendir(p);
 
         while (dirp)
         {
--- src/tools/mmaps_generator/PathGenerator.cpp
+++ src/tools/mmaps_generator/PathGenerator.cpp
@@ -235,14 +235,14 @@ bool handleArgs(int argc, char** argv,
 int finish(const char* message, int returnValue)
 {
     printf("%s", message);
-    getchar(); // Wait for user input
+    getchar();
     return returnValue;
 }
 
 int main(int argc, char** argv)
 {
     int threads = 3, mapnum = -1;
-    float maxAngle = 55.0f;
+    float maxAngle = 60.0f;
     int tileX = -1, tileY = -1;
     bool skipLiquid = false,
          skipContinents = false,
